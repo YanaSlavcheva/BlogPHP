b0a28c070401fbaa9542865022120eac606c2bca
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-04-29 17:40:21 +0300

    delete post deletes the link to tags as well

--- models/PostsModel.php
+++ models/PostsModel.php
@@ -2,16 +2,17 @@
 
 class PostsModel extends BaseModel {
     public function getAll() {
-        $statement = self::$db -> query("SELECT
-	p.post_id,
-    p.title,
-    p.content,
-    GROUP_CONCAT(tag_content ORDER BY tag_content ASC SEPARATOR ', ') AS tags
-FROM php_blog_system.posts p
-LEFT JOIN php_blog_system.posts_tags pt ON p.post_id = pt.post_id
-LEFT JOIN php_blog_system.tags t ON t.tag_id = pt.tag_id
-Group BY p.title, p.post_id
-ORDER BY p.post_id");
+        $statement = self::$db -> query(
+            "SELECT
+                p.post_id,
+                p.title,
+                p.content,
+                GROUP_CONCAT(tag_content ORDER BY tag_content ASC SEPARATOR ', ') AS tags
+            FROM php_blog_system.posts p
+            LEFT JOIN php_blog_system.posts_tags pt ON p.post_id = pt.post_id
+            LEFT JOIN php_blog_system.tags t ON t.tag_id = pt.tag_id
+            Group BY p.title, p.post_id
+            ORDER BY p.post_id");
         return $statement -> fetch_all(MYSQLI_ASSOC);
     }
 
@@ -49,9 +50,13 @@ ORDER BY p.post_id");
 
     public function delete($id) {
         $statement = self::$db -> prepare(
-            "DELETE FROM posts WHERE post_id = ?");
+            "DELETE pt, p
+            FROM posts_tags pt
+            INNER JOIN posts p ON pt.post_id = p.post_id
+            WHERE pt.post_id = ?");
         $statement -> bind_param("i", $id);
         $statement -> execute();
+
         return $statement -> affected_rows > 0;
     }
 }
