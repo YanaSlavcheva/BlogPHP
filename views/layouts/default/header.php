739d0684c9ca686771192b03a1ea525d1a068e9a
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-04-29 17:44:01 +0300

    books stuff cleared from the project

--- views/layouts/default/header.php
+++ views/layouts/default/header.php
@@ -12,7 +12,6 @@
         <ul class="menu">
             <li><a href="/">Home</a></li>
             <li><a href="/posts">Posts</a></li>
-            <li><a href="/books">Books</a></li>
         </ul>
     </header>
     <?php include_once('views/layouts/messages.php'); ?>
